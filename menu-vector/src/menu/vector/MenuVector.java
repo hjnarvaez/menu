7666db48947a55f545c158098bdb5f81f3156678
Hugo Narváez <hjnarvaez@tecnologicoitca.edu.ec>
2016-11-28 09:28:05 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class MenuVector {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         int[] A ;
         int[] B ;
         int[] C ;
-        a = new int[5]; 
-        
-        
-        
-        }
+        int limiteA,limiteB;
+        Scanner sc=new Scanner(System.in);
+        limiteA = sc.nextInt();
+        limiteB=sc.nextInt();
     }
     
 }
