0feeaeda9142785533f8a296f723adf205fc915f
Hugo Narváez <hjnarvaez@tecnologicoitca.edu.ec>
2016-11-28 09:14:11 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -3,12 +3,9 @@ package menu.vector;
 public class MenuVector {
 
     public static void main(String[] args) {
-        
-        Random rnd = new Random();
-        
         int[] A ;
         int[] B ;
- int[] C ;
+        int[] C ;
                 
         }
     }
