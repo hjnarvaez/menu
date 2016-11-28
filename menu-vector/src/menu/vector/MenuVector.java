39044051ac9dc70dc980b81a9f34dd36c5055d01
Cristian Obando <rafaelobando2015@hotmail.com>
2016-11-28 09:17:10 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -1,12 +1,14 @@
 package menu.vector;
 
+import java.util.Random;
+
 public class MenuVector {
 
     public static void main(String[] args) {
         int[] A ;
         int[] B ;
         int[] C ;
-                
+        a = new int[5];       
         }
     }
     
