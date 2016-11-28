a058ad000fee07db455b187c09f7ab5012669c39
edycala <pacala94@hotmail.com>
2016-11-28 09:31:48 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -12,6 +12,8 @@ public class MenuVector {
         Scanner sc=new Scanner(System.in);
         limiteA = sc.nextInt();
         limiteB=sc.nextInt();
+              for (int x=0;x<numeros.length;x++)
+  numeros[x] = (int) (Math.random()*9)+1;
     }
     
 }
