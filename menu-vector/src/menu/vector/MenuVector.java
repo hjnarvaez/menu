15f820160e1447ab421a4d2ebe57b8287d609008
Cabadiaz <gabrielcabascango7@gmail.com>
2016-11-28 09:21:28 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class RANDOMICO {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
         int[] A ;
         int[] B ;
         int[] C ;
-        a = new int[5];       
+        int[] a = new int[];
         }
     }
     
