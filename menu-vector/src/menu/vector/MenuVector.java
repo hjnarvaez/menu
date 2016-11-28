d8b992c404db417816f4f29222818086ef3cf73d
Hugo Narváez <hjnarvaez@tecnologicoitca.edu.ec>
2016-11-28 09:03:25 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -3,7 +3,7 @@ package menu.vector;
 public class MenuVector {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println((int)(Math.random()*6 + 1));
     }
     
 }
