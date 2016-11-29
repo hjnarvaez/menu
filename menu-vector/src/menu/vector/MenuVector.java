c2ace124e99809d68d8b626912bf6f8568eee88f
Franklin A. Herrera Padilla <fahp@outlook.es>
2016-11-28 23:27:12 -0500

    Update MenuVector.java

--- menu-vector/src/menu/vector/MenuVector.java
+++ menu-vector/src/menu/vector/MenuVector.java
@@ -15,5 +15,51 @@ public class MenuVector {
               for (int x=0;x<numeros.length;x++)
   numeros[x] = (int) (Math.random()*9)+1;
     }
-    
+     public void MostrarVector(int Vector[]) {
+        System.out.print("Vector = [ ");
+        for (int i = 0; i < Vector.length; i++) {
+            System.out.print(" " + Vector[i] + " ");
+        }
+        System.out.print(" ]\n");
+    }
+
+    public int TamañoVector() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del su vector:");
+        int tam = teclado.nextInt();
+        return tam;
+    }
+
+    public int[] IngresarVector(int Vector[]) {
+        for (int i = 0; i < Vector.length; i++) {
+            System.out.print("Ingrese valor en V[" + i + "]:");
+        }
+        return Vector;
+    }
+
+    public int MaxVector(int[] Vector) {
+
+        int Max = 1;
+
+        for (int i = 0; i < Vector.length; i++) {
+            if (Vector[i] > Max) {
+                Max = Vector[i];
+            }
+        }
+
+        return Max;
+    }
+
+    public int MimVector(int[] Vector) {
+
+        int Min;
+        Min = Vector[0];
+
+        for (int i = 0; i < Vector.length; i++) {
+            if (Vector[i] < Min) {
+                Min = Vector[i];
+            }
+        }
+        return Min;
+    }
 }
